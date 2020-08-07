50ace8d94c4644b543c507f87fd38116dc73c2ff
vinayak-gaikwad <58037628+vinayak-gaikwad@users.noreply.github.com>
2020-08-07 11:00:08 +0530

    Updates

---
+++ precache-manifest.4ead7a135280ce965e4174cc9038a0c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a0c5698432889aa8296826935f02b5",
+    "url": "/expense_tracker/index.html"
+  },
+  {
+    "revision": "e72f774cb2f95f3b5b89",
+    "url": "/expense_tracker/static/css/main.b334f5ee.chunk.css"
+  },
+  {
+    "revision": "cf15c901f86c2bb6ed5b",
+    "url": "/expense_tracker/static/js/2.a463868d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense_tracker/static/js/2.a463868d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72f774cb2f95f3b5b89",
+    "url": "/expense_tracker/static/js/main.6f1a78c5.chunk.js"
+  },
+  {
+    "revision": "49565c5e0bb05b50428c",
+    "url": "/expense_tracker/static/js/runtime-main.ca832478.js"
+  }
+]);
